988fdfe7e1608c144283a31550feb27851c4c3e5
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-08-27 15:01:49 +0200

    updated dotfiles

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-sudo apt-get install -y git-core vim build-essential htop ruby2.0 lxc lxc-templates cgroup-lite redir curl tmux
+sudo apt-get install -y git-core vim build-essential htop ruby2.0 lxc lxc-templates cgroup-lite redir curl tmux xfce4-terminal
 sudo apt-get update && sudo apt-get upgrade -y
 
 # node stuff
 curl -sL https://deb.nodesource.com/setup | sudo bash -
 sudo apt-get update
 sudo apt-get install nodejs -y
-sudo npm install -g grunt-cli bower
+#sudo npm install -g grunt-cli bower
 
 git clone https://github.com/fritzvd/dotfiles.git ~/.dotfiles
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -15,14 +15,17 @@ cd ~/.dotfiles
 ./script/bootstrap
 ./script/install
 
-mkdir -p .tmp
-cd .tmp
-wget "https://www.vagrantup.com/downloads"
-wget "`python ~/.dotfiles/latest_vagrant.py`"
-sudo dpkg -i *.deb
-vagrant plugin install vagrant-lxc
-rm -rf .tmp/
+#mkdir -p .tmp
+#cd .tmp
+#wget "https://www.vagrantup.com/downloads"
+#wget "`python ~/.dotfiles/latest_vagrant.py`"
+#sudo dpkg -i *.deb
+#vagrant plugin install vagrant-lxc
+#rm -rf .tmp/
 
-wget -O - https://bootstrap.pypa.io/get-pip.py | python
+wget -O - https://bootstrap.pypa.io/get-pip.py | sudo python
+
+# set xfce terminal
+gsettings set org.gnome.desktop.default-applications.terminal exec 'xfce4-terminal'
 
 exit;
